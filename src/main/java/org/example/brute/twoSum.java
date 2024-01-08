0403780beb3b2faa88dd1d407eb99b73a978b0ed
LEE JUNGWON <85065626+shoeone96@users.noreply.github.com>
2024-01-09 00:53:42 +0900

    TWO POINTER: Leet Code two sum

--- src/main/java/org/example/brute/twoSum.java
+++ src/main/java/org/example/brute/twoSum.java
@@ -1,36 +1,38 @@
 package org.example.brute;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-public class twoSum {
+class TwoSum {
 
     public int[] twoSum(int[] nums, int target) {
-        List<Integer> answer = new ArrayList<>();
-        boolean [] visited = new boolean[nums.length];
-        return backtracking(answer, nums, target, visited);
-    }
+        Map<Integer, List<Integer>> index = new HashMap<>();
+        for(int i = 0; i < nums.length; i++) {
+            index.putIfAbsent(nums[i], new ArrayList<>());
+            index.get(nums[i]).add(i);
+        }
 
-    private int [] backtracking(List<Integer> answer, int[] nums, int target, boolean[] visited) {
-        if(answer.size() == 2){
-            if(nums[answer.get(0)] + nums[answer.get(1)] == target){
-                return new int[]{answer.get(0), answer.get(1)};
-            }
-        } else {
-            for(int i = 0; i < nums.length; i++){
-                if(!visited[i]){
-                    answer.add(i);
-                    visited[i] = true;
-                    int[] backtracking = backtracking(answer, nums, target, visited);
-                    if(backtracking != null){
-                        return backtracking;
-                    }
-                    answer.remove(answer.size() -1);
-                    visited[i] = false;
+        Arrays.sort(nums);
+        int left = 0;
+        int right = nums.length - 1;
+
+        while(left < right) {
+            int sum = nums[left] + nums[right];
+            if(sum > target) {
+                right--;
+            } else if(sum < target) {
+                left++;
+            } else {
+                if(nums[left] == nums[right]) {
+                    return new int[]{index.get(nums[left]).get(0), index.get(nums[right]).get(1)};
+                } else {
+                    return new int[]{index.get(nums[left]).get(0), index.get(nums[right]).get(0)};
                 }
             }
         }
         return null;
     }
-
 }
