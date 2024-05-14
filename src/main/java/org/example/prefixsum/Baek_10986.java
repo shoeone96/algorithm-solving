a46704d76f3a31881ac1fdc9ec195a97d0ccc6f7
LEE JUNGWON <85065626+shoeone96@users.noreply.github.com>
2024-05-14 09:57:24 +0900

    PREFIX SUM: 백준 10986 시간초과

--- src/main/java/org/example/prefixsum/Baek_10986.java
+++ src/main/java/org/example/prefixsum/Baek_10986.java
@@ -7,34 +7,52 @@ import java.util.StringTokenizer;
 
 public class Baek_10986 {
 
+    static int N, M;
+    static int answer;
+    static int [] visited;
+    static int [] arr;
+    static int [] sum;
+
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
+        visited = new int[2];
+        answer = 0;
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        int answer = 0;
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-
-        int [] arr = new int[N];
+        arr = new int[N];
+        sum = new int[N + 1];
 
         st = new StringTokenizer(bf.readLine());
-        for(int i = 0; i <N; i++){
+        for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
+            sum[i + 1] = sum[i] + arr[i];
         }
-
-        for(int i = 0; i < N; i++){
-            int pointer = i;
-            int sum = 0;
-            while(pointer < N){
-                sum += arr[pointer];
-                if(sum % M == 0){
-                    answer++;
-                }
-                pointer++;
-            }
-        }
+        prod(0);
 
         System.out.println(answer);
+    }
+
+
+    static void prod(int count){
+        if(count == 2){
+            int part = sum[visited[1]] - sum[visited[0]];
+            if(part ==0){
+                return;
+            }
+            if(part % M == 0){
+                answer++;
+            }
+            return;
+        }
+        for(int i = 0; i <= N; i++){
+            if(count == 0 || visited[count-1] < i){
+                visited[count] = i;
+                prod(count + 1);
+                visited[count] = 0;
+            }
+        }
     }
 
 }
