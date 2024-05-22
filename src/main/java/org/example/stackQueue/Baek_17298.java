cecc443566a4a12fe0200b5244aeea5ee527b803
shoeone96 <dlwnddnjs96@gmail.com>
2024-05-22 10:14:51 +0900

    STACK: 백준 17298 stack으로 해결

--- src/main/java/org/example/stackQueue/Baek_17298.java
+++ src/main/java/org/example/stackQueue/Baek_17298.java
@@ -3,6 +3,8 @@ package org.example.stackQueue;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.StringTokenizer;
 
 public class Baek_17298 {
@@ -13,27 +15,36 @@ public class Baek_17298 {
 
         int n = Integer.parseInt(st.nextToken());
         int[] arr = new int[n];
+        int[] answerArr = new int[n];
 
         st = new StringTokenizer(bf.readLine());
         for (int i = 0; i < arr.length; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
         StringBuilder answer = new StringBuilder();
-        for (int i = 0; i < n; i++) {
-            int standard = arr[i];
-            int ngeIndex = i + 1;
-            while (true) {
-                if (ngeIndex == n) {
-                    answer.append(-1).append(" ");
-                    break;
-                }
-                if (arr[ngeIndex] > standard) {
-                    answer.append(arr[ngeIndex]).append(" ");
-                    break;
-                }
+        Deque<Integer> stack = new ArrayDeque<>();
 
-                ngeIndex++;
+        for (int i = 0; i < n; i++) {
+            if (i == 0) {
+                stack.push(i);
+                continue;
             }
+
+            while (!stack.isEmpty() && arr[stack.peek()] < arr[i]) {
+                Integer index = stack.pop();
+                answerArr[index] = arr[i];
+            }
+
+            stack.push(i);
+        }
+
+        while (!stack.isEmpty()) {
+            Integer leftIndex = stack.pop();
+            answerArr[leftIndex] = -1;
+        }
+
+        for (int i = 0; i < answerArr.length; i++) {
+            answer.append(answerArr[i]).append(" ");
         }
 
         System.out.println(answer);
