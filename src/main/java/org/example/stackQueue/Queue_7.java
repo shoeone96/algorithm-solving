916cf81029cd2cd72ae8dd3fe09a1184f2370391
shoeone96 <dlwnddnjs96@gmail.com>
2024-07-02 11:24:27 +0900

    QUEUE: inflearn

---
+++ src/main/java/org/example/stackQueue/Queue_7.java
@@ -0,0 +1,44 @@
+package org.example.stackQueue;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Queue_7 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        String essential = st.nextToken();
+        st = new StringTokenizer(bf.readLine());
+        String all = st.nextToken();
+
+        Queue<Character> essentialQueue = new LinkedList<>();
+        for(char ele : essential.toCharArray()){
+            essentialQueue.offer(ele);
+        }
+
+        Queue<Character> allQueue = new LinkedList<>();
+        for(char ele : all.toCharArray()){
+            allQueue.offer(ele);
+        }
+
+        while(!allQueue.isEmpty()){
+            Character poll = allQueue.poll();
+            if(poll.equals(essentialQueue.peek())){
+                essentialQueue.poll();
+            }
+        }
+
+        if(essentialQueue.isEmpty()){
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+
+}
