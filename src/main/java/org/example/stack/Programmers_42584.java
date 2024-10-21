106028734b38cb111058b6256349fec193c8ff80
shoeone96 <dlwnddnjs96@gmail.com>
2024-10-21 09:45:36 +0900

    프로그래머스 스택 주식 가격(모두 통과)

---
+++ src/main/java/org/example/stack/Programmers_42584.java
@@ -0,0 +1,32 @@
+package org.example.stack;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Programmers_42584 {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        Deque<Integer> stack = new ArrayDeque<>();
+        stack.push(0);
+        for(int i = 1; i < prices.length; i++){
+            if(!stack.isEmpty()){
+                while(!stack.isEmpty() && prices[stack.peek()] > prices[i]){
+                    int pop = stack.pop();
+                    answer[pop] = i - pop;
+                }
+            }
+
+            stack.push(i);
+        }
+
+        while(!stack.isEmpty()){
+            int pop = stack.pop();
+            if(pop == prices.length -1){
+                continue;
+            }
+            answer[pop] = prices.length -1 -pop;
+        }
+
+        return answer;
+    }
+}
