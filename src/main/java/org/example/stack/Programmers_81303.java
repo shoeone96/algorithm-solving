b26e115917b387741a9626f2c902280fa7dac134
shoeone96 <dlwnddnjs96@gmail.com>
2024-10-24 10:28:20 +0900

    1차 성공, 시간초과

--- src/main/java/org/example/stack/Programmers_81303.java
+++ src/main/java/org/example/stack/Programmers_81303.java
@@ -16,89 +16,88 @@ public class Programmers_81303 {
         String[] answerArr = new String[n];
         Deque<Integer> deletedStack = new ArrayDeque<>();
         Arrays.fill(answerArr, "O");
-        // 하나씩 순회
-        for (String command : cmd) {
-            // 각 명령어를 판단(움직임, 삭제/복원)
-            boolean isMove = isMove(command);
-            System.out.println(isMove);
 
-            if (isMove && command.startsWith("D")) { // 움직이면서 아래로 가는 경우
+        for (String command : cmd) {
+            if (isMove(command)) {
                 String[] commandArr = command.split(" ");
-                int move = Integer.parseInt(commandArr[commandArr.length - 1]);
-                k = down(answerArr, k, move);
-            } else if (isMove && command.startsWith("U")) { // 움직이면서 위로 가는 경우
-                String[] commandArr = command.split(" ");
-                int move = Integer.parseInt(commandArr[commandArr.length - 1]);
-                k = up(answerArr, k, move);
-            } else if (command.equals("C")) { //삭제하는 경우
+                char direction = command.charAt(0);
+                int move = Integer.parseInt(commandArr[1]);
+                if (direction == 'U') {
+                    k = up(answerArr, k, move);
+                } else {
+                    k = down(answerArr, k, move);
+                }
+            } else if (command.equals("C")) {
                 k = delete(answerArr, k, deletedStack);
-            } else { //복원하는 경우
+            } else if (command.equals("Z")) {
                 k = rollback(answerArr, k, deletedStack);
             }
-
         }
-
         return String.join("", answerArr);
     }
 
     private boolean isMove(String command) {
-        if (command.length() == 3) {
-            return true;
-        }
-
-        return false;
+        return command.startsWith("U") || command.startsWith("D");
     }
 
     private int up(String[] answerArr, int now, int move) {
+        int count = 0;
         int index = now;
-        for (int i = index; i > index - move; i--) {
-            if (answerArr[i].equals("X")) {
-                now = now - 2;
-            } else {
-                now--;
+        while (count < move && index > 0) {
+            index--;
+            if (answerArr[index].equals("O")) {
+                count++;
             }
         }
-        System.out.println(Arrays.toString(answerArr));
-        System.out.println(now);
-        return now;
+        return index;
     }
 
     private int down(String[] answerArr, int now, int move) {
+        int count = 0;
         int index = now;
-        for (int i = index; i < index + move; i++) {
-            if (answerArr[i].equals("X")) {
-                now = now + 2;
-            } else {
-                now++;
+        while (count < move && index < answerArr.length - 1) {
+            index++;
+            if (answerArr[index].equals("O")) {
+                count++;
             }
         }
-        System.out.println(Arrays.toString(answerArr));
-        System.out.println(now);
-        return now;
+        return index;
     }
 
     private int delete(String[] answerArr, int now, Deque<Integer> stack) {
         answerArr[now] = "X";
         stack.push(now);
-        if (now != answerArr.length - 1) {
-            now++;
+
+        if (now < answerArr.length - 1) {
+
+            int next = now + 1;
+            while (next < answerArr.length && answerArr[next].equals("X")) {
+                next++;
+            }
+            if (next < answerArr.length) {
+                now = next;
+            } else {
+                now--;
+                while (now >= 0 && answerArr[now].equals("X")) {
+                    now--;
+                }
+            }
         } else {
             now--;
+            while (now >= 0 && answerArr[now].equals("X")) {
+                now--;
+            }
         }
-        System.out.println(Arrays.toString(answerArr));
-        System.out.println(now);
         return now;
     }
 
     private int rollback(String[] answerArr, int now, Deque<Integer> stack) {
+        if (stack.isEmpty()) {
+            return now;
+        }
         int index = stack.pop();
         answerArr[index] = "O";
-        if (index < now) {
-            now++;
-        }
-        System.out.println(Arrays.toString(answerArr));
-        System.out.println(now);
+
         return now;
     }
-
 }
