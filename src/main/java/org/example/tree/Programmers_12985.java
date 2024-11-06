25239dcfda238d3ee09686b3ed305b24e7fa3901
shoeone96 <dlwnddnjs96@gmail.com>
2024-11-06 09:28:33 +0900

    프로그래머스 트리 예상대진표

---
+++ src/main/java/org/example/tree/Programmers_12985.java
@@ -0,0 +1,18 @@
+package org.example.tree;
+
+public class Programmers_12985 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int solution(int n, int a, int b) {
+        int answer = 0;
+        while (a != b) {
+            a = (a + 1) / 2;
+            b = (b + 1) / 2;
+            answer++;
+        }
+        return answer;
+    }
+}
